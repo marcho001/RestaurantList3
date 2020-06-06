a1f8eb8ebfbdc954557035b719bb7984ce50ca68
Marc <g40419@gmail.com>
2020-06-06 13:28:25 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const routes = require('./routes/index')
+//引入session
+const session = require('express-session')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
 
@@ -21,6 +23,12 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+//設定session secret resave saveunintialized
+app.use(session({
+  secret: 'getOffMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
