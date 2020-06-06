777063ef21380f2662be628da0da6905f72f573b
Marc <g40419@gmail.com>
2020-06-06 15:06:42 +0800

    feat: add authenticated feature in restaurant & home router

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const users = require('./modules/users')
 const restaurantList = require('./modules/restaurantList')
+//加入驗證程序 帶入middleware
+const { authenticator } = require('../middleware/auth')
 
-router.use('/', home)
-router.use('/restaurant', restaurantList)
+//加入後如果未登入會強制導去登入頁面
+router.use('/restaurant',authenticator, restaurantList)
 router.use('/users', users)
+//首頁router '/'要放在最後面 否則每個router都會被用到
+router.use('/',authenticator, home)
 
 module.exports = router
